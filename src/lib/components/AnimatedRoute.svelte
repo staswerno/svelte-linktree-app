bc82ec0f000c85b64054893e2de1e34bc3ab20ef
staswerno <staswerno@gmail.com>
2024-04-05 18:21:43 +0200

    add transitions

---
+++ src/lib/components/AnimatedRoute.svelte
@@ -0,0 +1,13 @@
+<script>
+        import { fly, fade, blur, slide, scale, draw, crossfade } from "svelte/transition";
+        import { quintIn, quintOut, backIn, backOut, circIn, circOut, expoIn, expoOut, sineIn, sineOut, linear } from 'svelte/easing';
+        import { page } from "$app/stores";
+</script>
+
+{#key $page.url}
+    <div in:blur={{ delay: 200, duration: 200, amount: 2, easing: sineIn }} out:blur={{ duration: 200, amount: 2, easing: sineOut }}  >
+        <slot />
+    </div>
+{/key}
+
+<!-- TODO: remove unused imports -->
--- src/routes/login/+layout.svelte
+++ src/routes/login/+layout.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
     import { page } from "$app/stores";
-
-    $: console.log($page.url);
+    import AnimatedRoute from "$lib/components/AnimatedRoute.svelte";
 </script>
 
 <nav class="flex justify-center my-6">
@@ -21,9 +20,11 @@
       </a>
     </ul>
   </nav>
-  
-<main class="card w-4/6 bg-neutral text-neutral-content mx-auto">
-    <div class="card-body items-center text-center">
-        <slot />
-    </div>
-</main>
+
+<AnimatedRoute>
+    <main class="card w-4/6 bg-neutral text-neutral-content mx-auto">
+        <div class="card-body items-center text-center">
+            <slot />
+        </div>
+    </main>
+    </AnimatedRoute>
