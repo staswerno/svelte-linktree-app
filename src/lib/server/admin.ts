1fc33ecba913d73f28d335593de82f496d9905ae
staswerno <staswerno@gmail.com>
2024-04-05 10:53:34 +0200

    fix admin.ts errors

--- src/lib/server/admin.ts
+++ src/lib/server/admin.ts
@@ -3,7 +3,8 @@
 
 import { getAuth } from 'firebase-admin/auth';
 import { getFirestore } from 'firebase-admin/firestore';
-import { FB_CLIENT_EMAIL, FB_PRIVATE_KEY, FB_PROJECT_ID, FB_SERVICE_ACCOUNT } from '$env/static/private'
+// TODO: add FB_SERVICE_ACCOUNT later? Currently producing an error
+import { FB_CLIENT_EMAIL, FB_PRIVATE_KEY, FB_PROJECT_ID } from '$env/static/private'
 import pkg from 'firebase-admin';
 
 try {
@@ -14,7 +15,7 @@ try {
       privateKey: FB_PRIVATE_KEY,
     }),
   });
-} catch (err) {
+} catch (err: any) {
   if (!/already exists/u.test(err.message)) {
     console.error('Firebase Admin Error: ', err.stack)
   }
