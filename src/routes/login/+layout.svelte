b785db35e4d773ed796d261d6e4147df7bc4eb94
staswerno <staswerno@gmail.com>
2024-04-05 17:09:38 +0200

    add step navigation

--- src/routes/login/+layout.svelte
+++ src/routes/login/+layout.svelte
@@ -1,7 +1,27 @@
 <script lang="ts">
+    import { page } from "$app/stores";
 
+    $: console.log($page.url);
 </script>
 
+<nav class="flex justify-center my-6">
+    <ul class="steps">
+      <a href="/login" class="step step-primary">sign in</a>
+      <a
+        href="/login/username"
+        class="step"
+        class:step-primary={$page.route.id?.includes("username") || $page.route.id?.includes("photo")}>
+        choose username
+      </a>
+      <a
+        href="/login/photo"
+        class="step"
+        class:step-primary={$page.route.id?.includes("photo")}>
+        upload photo
+      </a>
+    </ul>
+  </nav>
+  
 <main class="card w-4/6 bg-neutral text-neutral-content mx-auto">
     <div class="card-body items-center text-center">
         <slot />
