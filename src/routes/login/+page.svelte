8f3860c46e079eed16f2fb705281e2bb2c3601c3
staswerno <staswerno@gmail.com>
2024-04-15 17:34:36 +0200

    implement google auth

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,5 +1,23 @@
 <script lang="ts">
+// for larger projects, better to define sign in logic
+// in lib dir for use across multiple pages / limit
+// component complexity
 
+import { auth } from "$lib/firebase";
+import { GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+
+async function signInWithGoogle() {
+    const provider = new GoogleAuthProvider();
+    const user = await signInWithPopup(auth, provider);
+    console.log(provider)
+    console.log(auth)
+    // firebase issues json web token stored client side
+    // allowing app to verify that user is logged in.
+    // note: doesn't allow you to verify authentication
+    // status on the server. if needed, use cookie based
+    // authentication (to be implemented later)
+    console.log(user)
+}
 </script>
 
-<h1>login</h1>
+<button class="btn btn-primary btn-wide" on:click={signInWithGoogle}>sign in with google</button>
