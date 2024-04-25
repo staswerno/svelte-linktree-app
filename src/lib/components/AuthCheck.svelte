b5948d7b61e56849f219bec004661c2956ae7422
staswerno <staswerno@gmail.com>
2024-04-25 10:24:38 +0200

    update comment

--- src/lib/components/AuthCheck.svelte
+++ src/lib/components/AuthCheck.svelte
@@ -8,5 +8,6 @@
     <p class="mb-3">you must be signed in to view this page</p>
     <!-- TODO: Reset progress bar on click -->
     <!-- by moving it to the main layout? -->
+    <!-- or use a store? -->
     <a class="btn w-60" href="/login">sign in</a>
 {/if}
