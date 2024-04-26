eef2f5381c828f2e8683730fca906dd8898197f5
staswerno <staswerno@gmail.com>
2024-04-26 12:06:51 +0200

    fix auth check

--- src/lib/components/AuthCheck.svelte
+++ src/lib/components/AuthCheck.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
-  import { user } from "$lib/firebase";
+  import { user, loggedIn } from "$lib/firebase";
   import { progress, userClicked } from "$lib/stores/progress";
 </script>
 
-{#if $user}
+{#if $user && $loggedIn}
   <slot /> 
 {:else}
   <p class="mb-3">you must be signed in to view this page</p>
