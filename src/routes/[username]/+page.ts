6ed78cd397eeddaad3f6d82e5005e9152bcdcb37
staswerno <staswerno@gmail.com>
2024-05-02 12:27:08 +0200

    add markup

--- src/routes/[username]/+page.ts
+++ src/routes/[username]/+page.ts
@@ -12,13 +12,9 @@ export const load = (async ({ params }) => {
         where("username", "==", params.username),
         limit(1)
     )
-    // log these values to see what they are
     const snapshot = await getDocs(q);
     const exists = snapshot.docs[0]?.exists();
     const data = snapshot.docs[0]?.data();
-    console.log(snapshot);
-    console.log(exists);
-    console.log(data);
 
     if (!exists) {
         return error(404, "user not found");
@@ -29,7 +25,6 @@ export const load = (async ({ params }) => {
         // or... build catch block?
         throw error(403, `${data.username}'s profile is private`)
     }
-
 
     return {
         username: data.username,
