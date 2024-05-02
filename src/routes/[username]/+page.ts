8a6b8b8326ec76e8b46f8adb4bac73c3cf2b1809
staswerno <staswerno@gmail.com>
2024-05-02 11:40:45 +0200

    add document load function

---
+++ src/routes/[username]/+page.ts
@@ -0,0 +1,31 @@
+import { collection, getDocs, limit, query, where } from "firebase/firestore";
+import type { PageLoad } from "./$types";
+import { db } from "$lib/firebase";
+import { error } from "@sveltejs/kit";
+
+export const load = (async ({ params }) => {
+    
+    const collectionRef = collection(db, "users");
+
+    const q = query(
+        collectionRef,
+        where("username", "==", params.username),
+        limit(1)
+    )
+    // log these values to see what they are
+    const snapshot = await getDocs(q);
+    const exists = snapshot.docs[0]?.exists();
+    const data = snapshot.docs[0]?.data();
+    console.log(snapshot);
+    console.log(exists);
+    console.log(data);
+
+    
+
+    return {
+        username: data.username,
+        photoURL: data.photoURL,
+        bio: data.bio,
+        links: data.links ?? [],
+    };
+}) satisfies PageLoad;
