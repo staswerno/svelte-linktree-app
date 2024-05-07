fdfa488c1abef2242e437123fafb75096e7cff45
staswerno <staswerno@gmail.com>
2024-05-07 14:18:19 +0200

    add edit logic and markup

--- src/lib/components/UserLink.svelte
+++ src/lib/components/UserLink.svelte
@@ -1,5 +1,11 @@
-<script lang="ts">
-    import type { PageData } from './$types';
-    
-    export let data: PageData;
+<script>
+    export let icon = 'default';
+    export let url = 'foo';
+    export let title = 'some cool title';
 </script>
+
+<!-- TODO: style this -->
+<a href={url} class="stack w-full max-w-md text-center bg-base-300 flex justify-center items-center p-4 rounded-lg not-prose no-underline">
+  <img src={`/${icon}.png`} alt={icon} width="32" height="32" class="w-8" />
+  <span class="text-lg text-white font-bold">{title}</span>
+</a>
--- src/routes/[username]/+page.svelte
+++ src/routes/[username]/+page.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
+    import UserLink from '$lib/components/UserLink.svelte';
     import type { PageData } from './$types';
     
     export let data: PageData;
 </script>
 
 <svelte:head>
-    <title>{data.username} Links</title>
+    <title>{data.username} links</title>
     <meta name="description" content={data.bio} />
 </svelte:head>
   
@@ -27,9 +28,11 @@
 
   <p class="text-xl my-8">{data.bio ?? "bio coming soon..."}</p>
   <ul class="list-none">
-    <!-- {#each data.links as item}
-      {@debug item}
-    {/each} -->
+    {#each data.links as item}
+      <li>
+        <UserLink {...item} />
+      </li>
+    {/each}
   </ul>
 
 </main>
