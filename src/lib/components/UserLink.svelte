a113da42bc065416e2938ed3b8f22d06565bc7f9
staswerno <staswerno@gmail.com>
2024-05-23 17:50:52 +0200

    add icons

--- src/lib/components/UserLink.svelte
+++ src/lib/components/UserLink.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <a href={url} class="stack w-full max-w-md text-center bg-secondary flex justify-center items-center p-4 rounded-lg not-prose no-underline">
-    <!-- TODO: change icon url back to {`/${icon}.png`} -->
-  <img src={`https://cdn3.iconfinder.com/data/icons/feather-5/24/moon-128.png`} alt={icon} width="32" height="32" class="w-8" />
-  <span class="text-lg text-white font-bold">{title}</span>
+    <!-- TODO: find better icons -->
+    <img src={`/${icon}.png`} alt={icon} width="32" height="32" class="w-8" />
+    <span class="text-lg text-white font-bold">{title}</span>
 </a>
