d723565826137db58fbfb0cf2c6170017e6c4414
Stas Werno <89650713+staswerno@users.noreply.github.com>
2024-06-02 19:17:58 +0200

    tidy up sortable list

--- src/lib/components/SortableList.svelte
+++ src/lib/components/SortableList.svelte
@@ -2,6 +2,12 @@
     import { flip } from "svelte/animate";
     import { createEventDispatcher } from "svelte";
 
+    interface Item {
+        id: string;
+        index: number;
+        [key: string]: any;
+    }
+
     export let list: any[];
     let isOver: string | boolean = false;
 
@@ -23,7 +29,6 @@
 
   function onDragOver(e: DragEvent) {
     // @ts-ignore
-        const id = e.target.dataset?.id;
         const dragged = getDraggedParent(e.target);
         isOver = dragged?.id ?? false;
     }
@@ -44,6 +49,17 @@
 
     const reorder = ({ from, to }: any) => {
         const newList = [...list];
+        // EXPLANATION: brackets evaluated first
+        // brackets make to become from
+        // new array on right becomes (oldto, newto)
+        // then we assign oldto (using [0]) to oldfrom
+        // it's a bit like:
+        // newList = [1, oldfrom, 3, oldto, 5]
+        // ... = [..., (update oldto with oldfrom)]...
+        // newList = [1, oldfrom, 3, newto/oldfrom, 5]
+        // then:
+        // oldfrom = [oldto, newto][of these two items, update oldfrom with the first]
+        // newList = [1, newfrom/oldto, 3, newto/oldfrom, 5]
         newList[from] = [newList[to], (newList[to] = newList[from])][0];
 
         dispatch("sort", newList);
@@ -57,7 +73,7 @@
     <ul class="list-none p-0 flex flex-col items-center">
         {#each list as item, index (item.id)}
             <li
-                class="border-2 border-dashed border-transparent p-2 transition-all max-w-md w-full"
+                class="border-2 border-transparent p-2 transition-all max-w-md w-full"
                 class:over={item.id === isOver}
                 data-index={index}
                 data-id={item.id}
@@ -68,7 +84,13 @@
                 on:drop|preventDefault={onDrop}
                 animate:flip={{ duration: 300 }}
             >
-                <slot {item} {index} />
+                <slot {item}/>
+                <!-- TODO: are we actually using index? 
+                if there are problems, use below instead of above 
+                also, add back in
+                let:index
+                to component in username/edit-->
+                <!-- <slot {item} {index} /> -->
             </li>
         {/each}
     </ul>
