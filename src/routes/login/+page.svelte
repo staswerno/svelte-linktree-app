cf587bd14f70a11acb969f32731913bafb39f68f
staswerno <staswerno@gmail.com>
2024-08-01 13:13:06 +0200

    add session check code

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -6,13 +6,27 @@
   import { auth, user, userData, loggedIn } from "$lib/firebase";
   import { GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
   import { progress, userClicked } from "$lib/stores/progress"
+  import { onMount } from 'svelte';
+  import { browser } from '$app/environment';
   import AnimatedRoute from "$lib/components/AnimatedRoute.svelte";
   import AuthCheck from "$lib/components/AuthCheck.svelte";
   import Layout from "../+layout.svelte";
 
   export let data;
 
-  const uid = data.uid;
+  let uid = data.uid;
+
+  onMount(() => {
+    if (browser) {
+      const checkCookie = setInterval(() => {
+        // uid = document.cookie.replace(/(?:(?:^|.*;\s*)uid\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+        // uid = data.uid;
+        if (uid) {
+          clearInterval(checkCookie);
+        }
+      }, 100); // Check every 100ms
+    }
+  });
 
   async function signInWithGoogle() {
     const provider = new GoogleAuthProvider();
