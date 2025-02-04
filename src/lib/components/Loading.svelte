09093cb73b319c6db6032f9300c5a052c8290804
staswerno <staswerno@gmail.com>
2025-02-04 10:35:30 +0100

    fix formatting

--- src/lib/components/Loading.svelte
+++ src/lib/components/Loading.svelte
@@ -32,3 +32,6 @@
 {:else}
     <slot />
 {/if}
+
+<!-- formatting checked -->
+ 
--- src/lib/components/SignOutButton.svelte
+++ src/lib/components/SignOutButton.svelte
@@ -1,21 +1,23 @@
 <script lang="ts">
-    import { auth } from "$lib/firebase";
-    import { signOut } from "firebase/auth";
-    import { goto } from "$app/navigation";
+  import { auth } from "$lib/firebase";
+  import { signOut } from "firebase/auth";
+  import { goto } from "$app/navigation";
   
-    // set progress bar to 0
-    async function signOutSSR() {
-        const res = await fetch("/api/signin", { method: "DELETE" });
-        await signOut(auth);
-        goto("/login");
-    }
+  // set progress bar to 0
+  async function signOutSSR() {
+      const res = await fetch("/api/signin", { method: "DELETE" });
+      await signOut(auth);
+      goto("/login");
+  }
 
-    export let classes: string = "";
-  </script>
+  export let classes: string = "";
+</script>
 
-    <!-- SSR sign out below -->
-    <button class={`btn ${classes}`} on:click={signOutSSR}>sign out</button>
+<!-- SSR sign out below -->
+<button class={`btn ${classes}`} on:click={signOutSSR}>sign out</button>
 
-      
-    <!-- client side sign out below -->
-    <!-- <button class="btn w-60" on:click={() => signOut(auth)}>sign out</button> -->
+<!-- client side sign out below -->
+<!-- <button class="btn w-60" on:click={() => signOut(auth)}>sign out</button> -->
+
+<!-- formatting checked -->
+ 
